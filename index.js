07085fcc53fde355799dfaba14e743785689e5f7
bbennett7 <bryn.bennett@gmail.com>
2019-05-20 16:49:11 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+const var1 = 31;
+const var2 = 2;
+
+const multiply = var1 * var2;
+
+const random = Math.floor(Math.random() * 10) +1;
+
+const mod = 36 % 8;
+
+const max = Math.max(20);
