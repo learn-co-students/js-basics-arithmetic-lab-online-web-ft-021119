ffdd1d9d79bd842f94d9c4df14773c237a59997b
Christoph Gruener <christophgruener@me.com>
2019-05-21 15:38:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here
+
+let var1 = 6.2
+let var2 = 10
+
+let multiply = var1 * var2
+
+let random = Math.floor(Math.random()*5)
+
+let mod = 24%5
+
+let max = Math.max(2, 4, 6, 8, 20)
