250a3a7ac9e35a48cb4533c53735c73e79035640
Burak Kocak <burakkocak884@ghotmail.com>
2019-05-22 12:53:50 -0400

    Done.

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ const random = Math.random();
 const var3 = 19;
 const var4 = 5; 
 const mod  = var3 % var4;
-const numberList = [1,4,6,9,4,12,14,17,20];
+
 const max = Math.max(1,4,6,9,4,12,14,17,20);
 // Write your code here
