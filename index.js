8d5dd4750dd95f1298d9fc2d20df5bfd25596f23
brosie224 <barosengarten@gmail.com>
2019-06-13 00:16:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+const var1 = 31;
+const var2 = 2;
+const multiply = var1 * var2;
+
+const random = Math.floor(Math.random() * 100);
+
+const mod = 19 % 5;
+
+const max = Math.max(1, 2, 3, 20, 5);
